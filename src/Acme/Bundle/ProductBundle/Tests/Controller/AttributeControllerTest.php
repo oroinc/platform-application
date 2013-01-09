fd37b1e5ab27ac341c0a19e199b461d8ad729afc
Nicolas Dupont <nicolas@akeneo.com>
2013-01-09 14:58:08 +0100

    BAP-17 : move flexible entity demo bundle from flexible entity repo to bap-standard repo

---
+++ src/Acme/Bundle/ProductBundle/Tests/Controller/AttributeControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Acme\Bundle\ProductBundle\Tests\Controller;
+
+use Acme\Bundle\ProductBundle\Tests\Controller\AbstractControllerTest;
+
+/**
+ * Test related class
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/MIT MIT
+ *
+ */
+class AttributeControllerTest extends AbstractControllerTest
+{
+    /**
+     * Test related method
+     */
+    public function testInsert()
+    {
+        $this->client->request('GET', '/en/product/attribute/insert');
+
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Test related method
+     */
+    public function testIndex()
+    {
+        $this->client->request('GET', '/en/product/attribute/index');
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+}
