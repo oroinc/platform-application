c1c9ae62d6a728469db0b8407724db78132ff416
Romain Monceau <romain@akeneo.com>
2013-01-23 09:54:02 +0100

    BAP-38 : Implements measures converter with length family measure

---
+++ src/Oro/Bundle/MeasureBundle/Convert/MeasureConverter.php
@@ -0,0 +1,157 @@
+<?php
+namespace Oro\Bundle\MeasureBundle\Convert;
+
+use Oro\Bundle\MeasureBundle\Exception\UnknownFamilyMeasureException;
+
+use Oro\Bundle\MeasureBundle\Exception\UnknownMeasureException;
+
+use Oro\Bundle\MeasureBundle\Exception\UnknownOperatorException;
+
+/**
+ * Aims to convert measures
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/MIT MIT
+ *
+ */
+class MeasureConverter
+{
+
+    /**
+     * @var multitype
+     */
+    protected $config;
+
+    /**
+     * @var string
+     */
+    protected $family;
+
+    /**
+     * Constructor
+     * @param array $config
+     */
+    public function __construct($config = array())
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * Set a family for the converter
+     * @param string $family
+     *
+     * @return MeasureConverter
+     */
+    public function setFamily($family)
+    {
+        if (!isset($this->config[$family])) {
+            throw new UnknownFamilyMeasureException();
+        }
+
+        $this->family = $family;
+
+        return $this;
+    }
+
+    /**
+     * Convert a value from a base measure to a final measure
+     * @param string $baseUnit  Base unit for value
+     * @param string $finalUnit Result unit for value
+     * @param double $value     Value to convert
+     *
+     * @return double
+     */
+    public function convert($baseUnit, $finalUnit, $value)
+    {
+        $standardValue = $this->convertBaseToStandard($baseUnit, $value);
+
+        $result = $this->convertStandardToResult($finalUnit, $standardValue);
+
+        return $result;
+    }
+
+    /**
+     * Convert a value in a base unit to the standard unit
+     * @param string $baseUnit Base unit for value
+     * @param double $value    Value to convert
+     *
+     * @throws \Exception
+     *
+     * @return float
+     */
+    protected function convertBaseToStandard($baseUnit, $value)
+    {
+        if (!isset($this->config[$this->family]['units'][$baseUnit])) {
+            throw new UnknownMeasureException();
+        }
+        $conversionConfig = $this->config[$this->family]['units'][$baseUnit]['convert'];
+        $convertedValue = $value;
+
+        // calculate result with conversion config
+        foreach ($conversionConfig as $operator => $operand) {
+            switch ($operator) {
+                case "/":
+                    if ($operand !== 0) {
+                        $convertedValue = $convertedValue / $operand;
+                    }
+                    break;
+                case "*":
+                    $convertedValue = $convertedValue * $operand;
+                    break;
+                case "+":
+                    $convertedValue = $convertedValue + $operand;
+                    break;
+                case "-":
+                    $convertedValue = $convertedValue - $operand;
+                    break;
+                default:
+                    throw new UnknownOperatorException();
+            }
+        }
+
+        return $convertedValue;
+    }
+
+    /**
+     * Convert a value in a standard unit to a final unit
+     * @param string $finalUnit Final unit for value
+     * @param double $value     Value to convert
+     *
+     * @throws \Exception
+     * @return double
+     */
+    protected function convertStandardToResult($finalUnit, $value)
+    {
+        if (!isset($this->config[$this->family]['units'][$finalUnit])) {
+            throw new UnknownMeasureException();
+        }
+        $conversionConfig = $this->config[$this->family]['units'][$finalUnit]['convert'];
+        $convertedValue = $value;
+
+        // calculate result with conversion config
+        foreach ($conversionConfig as $operator => $rate) {
+            switch ($operator) {
+                case "/":
+                    if ($operand !== 0) {
+                        $convertedValue = $convertedValue * $rate;
+                    }
+                    break;
+                case "*":
+                    $convertedValue = $convertedValue / $rate;
+                    break;
+                case "+":
+                    $convertedValue = $convertedValue - $rate;
+                    break;
+                case "-":
+                    $convertedValue = $convertedValue + $rate;
+                    break;
+                default:
+                    throw new UnknownOperatorException();
+            }
+        }
+
+        return $convertedValue;
+    }
+
+}
