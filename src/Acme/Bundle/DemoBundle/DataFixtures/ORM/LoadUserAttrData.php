07d7343d66ab4da8884683495533e683f5622a02
magedan <danil@magecore.com>
2013-01-23 16:51:49 +0200

    Flexible attributes set for User entity

--- src/Acme/Bundle/DemoBundle/DataFixtures/ORM/LoadUserAttrData.php
+++ src/Acme/Bundle/DemoBundle/DataFixtures/ORM/LoadUserAttrData.php
@@ -2,13 +2,27 @@
 
 namespace Acme\Bundle\DemoBundle\DataFixtures\ORM;
 
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-use Oro\Bundle\UserBundle\Entity\Group;
+use Oro\Bundle\FlexibleEntityBundle\Model\AbstractAttributeType;
+use Oro\Bundle\UserBundle\Entity\User;
 
-class LoadGroupData extends AbstractFixture
+class LoadUserAttrData extends AbstractFixture implements ContainerAwareInterface
 {
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     /**
      * Load sample user group data
      *
@@ -16,10 +30,51 @@ class LoadGroupData extends AbstractFixture
      */
     public function load(ObjectManager $manager)
     {
-        $manager->persist(new Group('Users', ['ROLE_USER']));
-        $manager->persist(new Group('Managers', ['ROLE_USER']));
-        $manager->persist(new Group('Administrators', ['ROLE_ADMIN', 'ROLE_SUPER_ADMIN']));
+        /**
+         * @var Oro\Bundle\FlexibleEntityBundle\Manager\FlexibleEntityManager
+         */
+        $fm = $this->container->get('oro_user.flexible_manager');
+        $sm = $fm->getStorageManager();
 
-        $manager->flush();
+        $attr = $fm->createAttribute()
+            ->setCode('firstname')
+            ->setBackendType(AbstractAttributeType::BACKEND_TYPE_VARCHAR);
+
+        $sm->persist($attr);
+
+        $attr = $fm->createAttribute()
+            ->setCode('lastname')
+            ->setBackendType(AbstractAttributeType::BACKEND_TYPE_VARCHAR);
+
+        $sm->persist($attr);
+
+        $attr = $fm->createAttribute()
+            ->setCode('salary')
+            ->setBackendType(AbstractAttributeType::BACKEND_TYPE_INTEGER);
+
+        $sm->persist($attr);
+
+        $attr = $fm->createAttribute()
+            ->setCode('birthday')
+            ->setBackendType(AbstractAttributeType::BACKEND_TYPE_DATE);
+
+        $sm->persist($attr);
+
+        $attr = $fm->createAttribute()
+            ->setCode('gender')
+            ->setBackendType(AbstractAttributeType::BACKEND_TYPE_OPTION)
+            ->addOption(
+                $fm->createAttributeOption()->addOptionValue(
+                    $fm->createAttributeOptionValue()->setValue('Male')
+                )
+            )
+            ->addOption(
+                $fm->createAttributeOption()->addOptionValue(
+                    $fm->createAttributeOptionValue()->setValue('Female')
+                )
+            );
+
+        $sm->persist($attr);
+        $sm->flush();
     }
 }
