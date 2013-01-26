9101843371e71f5c048a6835d0191948cce451a6
Nicolas Dupont <nicolas@akeneo.com>
2013-01-26 03:07:46 +0100

    BAP-46 : add flexible customer CRUD action and base form type

--- src/Acme/Bundle/DemoFlexibleEntityBundle/Controller/CustomerController.php
+++ src/Acme/Bundle/DemoFlexibleEntityBundle/Controller/CustomerController.php
@@ -1,7 +1,9 @@
 <?php
-
 namespace Acme\Bundle\DemoFlexibleEntityBundle\Controller;
 
+use Oro\Bundle\FlexibleEntityBundle\Manager\FlexibleEntityManager;
+use Acme\Bundle\DemoFlexibleEntityBundle\Form\Type\CustomerType;
+use Acme\Bundle\DemoFlexibleEntityBundle\Entity\Customer;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -90,6 +92,69 @@ class CustomerController extends Controller
         $customer = $this->getCustomerManager()->getEntityRepository()->findWithAttributes($id);
 
         return array('customer' => $customer);
+    }
+
+
+    /**
+     * Create customer
+     *
+     * @Route("/create")
+     * @Template("AcmeDemoFlexibleEntityBundle:Customer:edit.html.twig")
+     */
+    public function createAction()
+    {
+        $attribute = $this->getCustomerManager()->createEntity();
+
+        return $this->editAction($attribute);
+    }
+
+    /**
+     * Edit customer
+     *
+     * @Route("/edit/{id}", requirements={"id"="\d+"}, defaults={"id"=0})
+     * @Template
+     */
+    public function editAction(Customer $entity)
+    {
+        $request = $this->getRequest();
+
+        // create form
+        $entClassName = $this->getCustomerManager()->getEntityName();
+        $form = $this->createForm(new CustomerType($entClassName), $entity);
+
+        if ($request->getMethod() == 'POST') {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $em = $this->getCustomerManager()->getStorageManager();
+                $em->persist($entity);
+                $em->flush();
+
+                $this->get('session')->getFlashBag()->add('success', 'Customer successfully saved');
+
+                return $this->redirect($this->generateUrl('acme_demoflexibleentity_customer_index'));
+            }
+        }
+
+        return array(
+            'form' => $form->createView(),
+        );
+    }
+
+    /**
+     * Remove customer
+     *
+     * @Route("/remove/{id}", requirements={"id"="\d+"})
+     */
+    public function removeAction(Customer $entity)
+    {
+        $em = $this->getCustomerManager()->getStorageManager();
+        $em->remove($entity);
+        $em->flush();
+
+        $this->get('session')->getFlashBag()->add('success', 'Customer successfully removed');
+
+        return $this->redirect($this->generateUrl('acme_demoflexibleentity_customer_index'));
     }
 
 
