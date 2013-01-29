3669ec4a466b9a0d32b628dd58e9e76e90383dac
Romain Monceau <romain@akeneo.com>
2013-01-29 15:38:50 +0100

    BAP-46 : Add tests

--- src/Acme/Bundle/DemoFlexibleEntityBundle/Tests/Controller/CustomerControllerTest.php
+++ src/Acme/Bundle/DemoFlexibleEntityBundle/Tests/Controller/CustomerControllerTest.php
@@ -119,31 +119,30 @@ class CustomerControllerTest extends KernelAwareControllerTest
      */
     public function testCreateAction()
     {
-        // count customers in database
-        $countCustomers = $this->countCustomers();
-
         // just call view to show form
-        $this->client->request('GET', self::prepareUrl('en', 'create'));
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
-
-        // post data to create a customer entity
-        $postData = array(
-            'firstname' => 'Test',
-            'lastname' => 'Test',
-            'email' => 'mail@mail.com'
-        );
-        $this->client->request('POST', self::prepareUrl('en', 'create'), $postData);
-        var_dump($this->client->getResponse()->getContent());
-        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+        foreach (self::$locales as $locale) {
+            $this->client->request('GET', self::prepareUrl($locale, 'create'));
+            $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        }
     }
 
     /**
      * Test related method
      */
-//     public function testEditAction()
-//     {
-//         // count customers in database
-//     }
+    public function testEditAction()
+    {
+        // find customer to edit
+        $customer = $this->getCustomerManager()->getEntityRepository()->findOneBy(array());
+        if (!$customer) {
+            throw new \Exception('Customer not found');
+        }
+
+        // just call view to show form
+        foreach (self::$locales as $locale) {
+            $this->client->request('GET', self::prepareUrl($locale, 'edit/'. $customer->getId()));
+            $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        }
+    }
 
     /**
      * Count customers in database
@@ -157,54 +156,114 @@ class CustomerControllerTest extends KernelAwareControllerTest
         return count($customers);
     }
 
-//     /**
-//      * Test query actions
-//      */
-//     public function testQueries()
-//     {
-//         $actions = array(
-//             'querylazyload',
-//             'queryonlydob',
-//             'queryonlydobandgender',
-//             'queryfilterfirstname',
-//             'queryfilterfirstnameandcompany',
-//             'queryfilterfirstnameandlimit',
-//             'queryfilterfirstnameandorderbirthdatedesc'
-//         );
+    /**
+     * Test related method
+     */
+    public function testQueryLazyLoadAction()
+    {
+        foreach (self::$locales as $locale) {
+            $this->client->request('GET', self::prepareUrl($locale, 'query-lazy-load'));
+            $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        }
+    }
 
-//         foreach ($actions as $action) {
-//             $this->client->request('GET', self::prepareUrl('en', $action));
-//             $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
-//         }
-//     }
+    /**
+     * Related method
+     */
+    public function testQueryAction()
+    {
+        // test with all parameters to null
+        $params = array(
+            'attributes' => 'null',
+            'criteria'   => 'null',
+            'orderBy'    => 'null',
+            'limit'      => 'null',
+            'offset'     => 'null'
+        );
+        $this->callQueryActionUrl($params);
 
-//     /**
-//      * Test query actions without data
-//      */
-//     public function testQueriesWithoutData()
-//     {
-//         // actions returning code 200
-//         $actions = array(
-//             'querylazyload',
-//             'queryfilterfirstname',
-//             'queryfilterfirstnameandlimit',
-//         );
-//         foreach ($actions as $action) {
-//             $this->client->request('GET', self::prepareUrl('en', $action));
-//             $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
-//         }
+        // test with only dob
+        $params = array(
+            'attributes' => 'dob'
+        );
+        $this->callQueryActionUrl($params);
 
-//         // actions returning exception
-//         $actions = array(
-//             'queryonlydob',
-//             'queryonlydobandgender',
-//             'queryfilterfirstnameandcompany',
-//             'queryfilterfirstnameandorderbirthdatedesc'
+        // test with dob and gender
+        $params = array(
+            'attributes' => 'dob&gender'
+        );
+        $this->callQueryActionUrl($params);
+
+        // test filtered by firstname
+        $params = array(
+            'attributes' => 'null',
+            'criteria'   => 'firstname=Nicolas'
+        );
+        $this->callQueryActionUrl($params);
+
+        // test filtered by firstname and company
+//         $params = array(
+//             'criteria'   => 'firstname=Romain&company=Akeneo'
 //         );
-//         foreach ($actions as $action) {
-//             $this->client->request('GET', self::prepareUrl('en', $action));
-//             $this->assertEquals(500, $this->client->getResponse()->getStatusCode());
-//         }
-//     }
+//         $this->callQueryActionUrl($params);
+
+        // test dob, company and gender filtered by firstname and company
+        $params = array(
+            'attributes' => 'dob&company&gender',
+            'criteria'   => 'firstname=Romain&company=Akeneo'
+        );
+        $this->callQueryActionUrl($params);
+
+        // test filtered by firstname and limit
+        $params = array(
+            'criteria' => 'firstname=Nicolas',
+            'limit'    => 10,
+            'offset'   => 0
+        );
+        $this->callQueryActionUrl($params);
+
+        // test select dob filtered by firstname and order by dob desc
+        $params = array(
+            'attributes' => 'dob',
+            'criteria'   => 'firstname=Romain',
+            'orderBy'    => 'dob=desc'
+        );
+        $this->callQueryActionUrl($params);
+    }
+
+    /**
+     * Call query action and assert result
+     * @param multitype $params
+     */
+    protected function callQueryActionUrl($params)
+    {
+        $attributes = (isset($params['attributes'])) ? $params['attributes'] : 'null';
+        $criteria   = (isset($params['criteria'])) ? $params['criteria'] : 'null';
+        $orderBy    = (isset($params['orderBy'])) ? $params['orderBy'] : 'null';
+        $limit      = (isset($params['limit'])) ? $params['limit'] : 'null';
+        $offset     = (isset($params['offset'])) ? $params['offset'] : 'null';
+
+        $urlSuffix = $this->prepareUrlForQueryAction($attributes, $criteria, $orderBy, $limit, $offset);
+        foreach (self::$locales as $locale) {
+            $this->client->request('GET', self::prepareUrl($locale, 'query/'. $urlSuffix));
+            $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        }
+    }
+
+    /**
+     * Prepare tested url with parameters
+     *
+     * @param string $attributes attribute codes
+     * @param string $criteria   criterias
+     * @param string $orderBy    order by
+     * @param int    $limit      limit
+     * @param int    $offset     offset
+     *
+     * @return string
+     */
+    protected function prepareUrlForQueryAction($attributes, $criteria, $orderBy, $limit, $offset)
+    {
+        return $attributes .'/'. $criteria .'/'. $orderBy .'/'. $limit .'/'. $offset;
+    }
 
 }
