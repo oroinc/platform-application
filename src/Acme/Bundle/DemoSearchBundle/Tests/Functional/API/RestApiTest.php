f58ad04275441f7b9d19280a2eefb37658df76d2
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-02-07 17:53:04 +0200

    BAP-73 Search API Tests Coverage  - added new Acme DemoSearchBundle

---
+++ src/Acme/Bundle/DemoSearchBundle/Tests/Functional/API/RestApiTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Oro\Bundle\SearchBundle\Test\Functional\Api;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RestApiTest extends WebTestCase
+{
+    protected $_client;
+
+    public function setUp()
+    {
+        $this->_client = static::createClient();
+    }
+
+    public function testApi()
+    {
+        $this->_client->request('GET', 'api/rest/latest/search');
+
+        $response = $this->_client->getResponse();
+
+        $this->assertJsonResponse($response, 200);
+    }
+
+    protected function tearDown()
+    {
+        unset($this->_client);
+    }
+
+    protected function assertJsonResponse($response, $statusCode = 200)
+    {
+        $this->assertEquals(
+            $statusCode, $response->getStatusCode(),
+            $response->getContent()
+        );
+        $this->assertTrue(
+            $response->headers->contains('Content-Type', 'application/json'),
+            $response->headers
+        );
+    }
+
+}
