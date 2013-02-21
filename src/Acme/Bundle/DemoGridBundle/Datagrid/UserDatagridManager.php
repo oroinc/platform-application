c5b821871537abbd8a5cacb358b8e3a4050f70b1
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2013-02-21 19:10:06 +0200

    BAP-111: Implement basic filter functionality - modified source data structure in user datagrid manager

--- src/Acme/Bundle/DemoGridBundle/Datagrid/UserDatagridManager.php
+++ src/Acme/Bundle/DemoGridBundle/Datagrid/UserDatagridManager.php
@@ -4,58 +4,101 @@ namespace Acme\Bundle\DemoGridBundle\Datagrid;
 
 use Oro\Bundle\GridBundle\Datagrid\FlexibleDatagridManager;
 use Oro\Bundle\GridBundle\Field\FieldDescription;
+use Oro\Bundle\GridBundle\Field\FieldDescriptionCollection;
 
 class UserDatagridManager extends FlexibleDatagridManager
 {
     /**
-     * @var array
+     * @var FieldDescriptionCollection
      */
-    protected $datagridFields = array(
-        'id' => array(
-            'label'      => 'ID',
-            'type'       => 'oro_grid_orm_number',
-            'field_type' => 'number',
-            'field_name' => 'id',
-            'sortable'   => true
-        ),
-        'username' => array(
-            'label'      => 'Username',
-            'type'       => 'oro_grid_orm_string',
-            'field_type' => 'text',
-            'field_name' => 'username',
-            'sortable'   => true
-        ),
-        'email' => array(
-            'label'      => 'Email',
-            'type'       => 'oro_grid_orm_string',
-            'field_type' => 'text',
-            'field_name' => 'email',
-            'sortable'   => true
-        )
-    );
+    protected $fieldsCollection;
+
+    /**
+     * @return FieldDescriptionCollection
+     */
+    protected function getFieldDescriptionCollection()
+    {
+        if (!$this->fieldsCollection) {
+            $this->fieldsCollection = new FieldDescriptionCollection();
+
+            $fieldId = new FieldDescription();
+            $fieldId->setName('id');
+            $fieldId->setOptions(
+                array(
+                    'type'        => 'number',
+                    'label'       => 'ID',
+                    'field_type'  => 'number',
+                    'field_name'  => 'id',
+                    'filter_type' => 'oro_grid_orm_number',
+                    'required'    => false,
+                    'sortable'    => true,
+                )
+            );
+            $this->fieldsCollection->add($fieldId);
+
+            $fieldUsername = new FieldDescription();
+            $fieldUsername->setName('username');
+            $fieldUsername->setOptions(
+                array(
+                    'type'        => 'text',
+                    'label'       => 'Username',
+                    'field_type'  => 'text',
+                    'field_name'  => 'username',
+                    'filter_type' => 'oro_grid_orm_string',
+                    'required'    => false,
+                    'sortable'    => true,
+                )
+            );
+            $this->fieldsCollection->add($fieldUsername);
+
+            $fieldEmail = new FieldDescription();
+            $fieldEmail->setName('email');
+            $fieldEmail->setOptions(
+                array(
+                    'type'        => 'text',
+                    'label'       => 'Email',
+                    'field_type'  => 'text',
+                    'field_name'  => 'email',
+                    'filter_type' => 'oro_grid_orm_string',
+                    'required'    => false,
+                    'sortable'    => true,
+                )
+            );
+            $this->fieldsCollection->add($fieldEmail);
+
+            foreach ($this->getFlexibleAttributes() as $attribute) {
+                $filterType = $attribute->getBackendType() == 'options'
+                    ? 'oro_grid_orm_flexible_options'
+                    : 'oro_grid_orm_flexible_string';
+                $sortable = $attribute->getBackendType() != 'options';
+
+                $field = new FieldDescription();
+                $field->setName($attribute->getCode());
+                $field->setOptions(
+                    array(
+                        'type'          => $attribute->getBackendType(),
+                        'label'         => $attribute->getCode(),
+                        'field_type'    => $attribute->getBackendType(),
+                        'field_name'    => $attribute->getCode(),
+                        'filter_type'   => $filterType,
+                        'required'      => false,
+                        'sortable'      => $sortable,
+                        'flexible_name' => $this->flexibleManager->getFlexibleName()
+                    )
+                );
+                $this->fieldsCollection->add($field);
+            }
+        }
+
+        return $this->fieldsCollection;
+    }
 
     /**
      * {@inheritdoc}
      */
     protected function getListFields()
     {
-        $fields = array();
-        foreach ($this->datagridFields as $fieldName => $fieldParameters) {
-            $field = new FieldDescription();
-            $field->setName($fieldName);
-            $field->setOption('label', $fieldParameters['label']);
-            $fields[] = $field;
-        }
-
-        foreach ($this->getFlexibleAttributes() as $attribute) {
-            $field = new FieldDescription();
-            $field->setName($attribute->getCode());
-            $field->setOption('label', $attribute->getCode());
-            $field->setType($attribute->getBackendType());
-            $fields[] = $field;
-        }
-
-        return $fields;
+        return $this->getFieldDescriptionCollection()->getElements();
     }
 
     /**
@@ -64,19 +107,11 @@ class UserDatagridManager extends FlexibleDatagridManager
     protected function getSorters()
     {
         $fields = array();
-        foreach ($this->datagridFields as $fieldName => $fieldParameters) {
-            if ($fieldParameters['sortable']) {
-                $field = new FieldDescription();
-                $field->setName($fieldName);
-                $fields[] = $field;
+        /** @var $fieldDescription FieldDescription */
+        foreach ($this->getFieldDescriptionCollection() as $fieldDescription) {
+            if ($fieldDescription->getOption('sortable')) {
+                $fields[] = $fieldDescription;
             }
-        }
-
-        foreach ($this->getFlexibleAttributes() as $attribute) {
-            $field = new FieldDescription();
-            $field->setName($attribute->getCode());
-            $field->setOption('flexible_name', $this->flexibleManager->getFlexibleName());
-            $fields[] = $field;
         }
 
         return $fields;
@@ -87,35 +122,6 @@ class UserDatagridManager extends FlexibleDatagridManager
      */
     protected function getFilters()
     {
-        $fields = array();
-        foreach ($this->datagridFields as $fieldName => $fieldParameters) {
-            $field = new FieldDescription();
-            $field->setName($fieldName);
-            $field->setType($fieldParameters['type']);
-            $field->setOption('label', $fieldParameters['label']);
-            $field->setOption('field_type', $fieldParameters['field_type']);
-            $field->setOption('field_name', $fieldParameters['field_name']);
-            $field->setOption('required', false);
-            $fields[] = $field;
-        }
-
-        foreach ($this->getFlexibleAttributes() as $attribute) {
-            $filterType = $attribute->getBackendType() == 'options'
-                ? 'oro_grid_orm_flexible_options'
-                : 'oro_grid_orm_flexible_string';
-
-            $field = new FieldDescription();
-            $field->setName($attribute->getCode());
-
-            $field->setType($filterType);
-            $field->setOption('label', $attribute->getCode());
-            $field->setOption('field_type', $attribute->getBackendType());
-            $field->setOption('field_name', $attribute->getCode());
-            $field->setOption('flexible_name', $this->flexibleManager->getFlexibleName());
-            $field->setOption('required', false);
-            $fields[] = $field;
-        }
-
-        return $fields;
+        return $this->getFieldDescriptionCollection()->getElements();
     }
 }
