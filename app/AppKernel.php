999921b284093f973bd1a8a7c19bf030793888ce
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-02-26 16:19:02 +0200

    BAP-97 Prepare Bamboo environment for performance test  - added performance environment

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -45,7 +45,7 @@ class AppKernel extends Kernel
             new Acme\Bundle\DemoFlexibleEntityBundle\AcmeDemoFlexibleEntityBundle()
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test', 'perf'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
