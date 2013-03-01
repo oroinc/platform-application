a9ac94bc161baa5b688c9b71ea066128d062a52c
Dmitry Khrysev <dkhrysev@magecore.com>
2013-03-01 15:40:59 +0200

    BAP-128: Pinbar button functonality added

--- src/Acme/Bundle/DemoPinbarBundle/Resources/public/js/views/pinbar.js
+++ src/Acme/Bundle/DemoPinbarBundle/Resources/public/js/views/pinbar.js
@@ -7,28 +7,65 @@ pinbar.MainView = Backbone.View.extend({
         el: '.pin-bar',
         listBar: '.list-bar',
         tabsTab: '#tabs-tab',
-        tabsContent: '#tabs-content'
+        pinIcon: '.pin-btn'
     },
 
     requireCleanup: true,
     massAdd: false,
 
-    emptyTemplate: _.template($("#template-no-pins-message").html()),
+    templates: {
+        noItemsMessage: _.template($("#template-no-pins-message").html())
+    },
 
     initialize: function() {
         this.$listBar = this.getBackboneElement(this.options.listBar);
         this.$tabs = this.getBackboneElement(this.options.tabsTab);
         this.$tabsBar = this.getBackboneElement(this.options.tabsContent);
+        this.$pinIcon = Backbone.$(this.options.pinIcon);
 
         this.listenTo(pinbar.Items, 'add', function(item) {this.setItemPosition(item)}.bind(this));
         this.listenTo(pinbar.Items, 'remove', this.reorder);
         this.listenTo(pinbar.Items, 'positionChange', this.renderItem);
         this.listenTo(pinbar.Items, 'reset', this.addAll);
         this.listenTo(pinbar.Items, 'all', this.render);
+        this.$pinIcon.click(this.togglePagePin.bind(this))
     },
 
     getBackboneElement: function(el) {
         return el instanceof Backbone.$ ? el : this.$(el);
+    },
+
+    togglePagePin: function(e) {
+        var itemData = this.getCurrentPageItemData()
+        var pinnedItem = pinbar.Items.where(itemData);
+        if (pinnedItem.length) {
+            _.each(pinnedItem, function(item) {item.destroy()});
+            this.removePagePinStatus();
+        } else {
+            var el = Backbone.$(e.currentTarget);
+            if (el.data('uri')) {
+                itemData['uri'] = el.data('uri');
+            }
+            itemData['title'] = el.data('title') ? el.data('title') : document.title;
+            var currentItem = new pinbar.Item(itemData);
+            //currentItem.save();
+            pinbar.Items.unshift(currentItem);
+            this.addPagePinStatus();
+        }
+    },
+
+    addPagePinStatus: function() {
+        this.$pinIcon.addClass('btn-success');
+        this.$pinIcon.find('i').addClass('icon-white');
+    },
+
+    removePagePinStatus: function() {
+        this.$pinIcon.removeClass('btn-success');
+        this.$pinIcon.find('i').removeClass('icon-white');
+    },
+
+    getCurrentPageItemData: function() {
+        return {uri: window.location.pathname};
     },
 
     addAll: function() {
@@ -89,7 +126,7 @@ pinbar.MainView = Backbone.View.extend({
         if (!this.massAdd) {
             if (pinbar.Items.length == 0) {
                 this.requireCleanup = true;
-                this.$listBar.html(this.emptyTemplate());
+                this.$listBar.html(this.templates.noItemsMessage());
             }
             if (pinbar.Items.length > this.options.maxPinbarItems) {
                 this.$tabs.show();
