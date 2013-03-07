4c57d163bdf22b834d52b745ff8f09a2bf1d466d
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2013-03-07 15:30:38 +0200

    BAP-135: Implement AJAX grid using backbons.js  - added fixture to load product data  - added demo for grid with custom DQL query

---
+++ src/Acme/Bundle/DemoGridBundle/Controller/ProductController.php
@@ -0,0 +1,68 @@
+<?php
+namespace Acme\Bundle\DemoGridBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Oro\Bundle\GridBundle\Datagrid\ORM\QueryFactory\QueryFactory;
+
+use Acme\Bundle\DemoGridBundle\Datagrid\UserDatagridManager;
+
+/**
+ * @Route("/product")
+ */
+class ProductController extends Controller
+{
+    /**
+     * @Route("/list.{_format}",
+     *      name="acme_demo_gridbundle_product_list",
+     *      requirements={"_format"="html|json"},
+     *      defaults={"_format" = "html"}
+     * )
+     */
+    public function listAction(Request $request)
+    {
+        /** @var $em \Doctrine\ORM\EntityManager */
+        $em = $this->getDoctrine()->getManager();
+        $queryBuilder = $em->createQueryBuilder();
+        $queryBuilder
+            ->select(
+                'm.id AS m_id',
+                'm.name AS m_name',
+                'p.id',
+                'p.name',
+                'p.count',
+                'p.price',
+                'p.description',
+                'p.createDate',
+                'COUNT(p.id) AS product_count'
+            )
+            ->from('AcmeDemoBundle:Manufacturer', 'm')
+            ->leftJoin('m.products', 'p')
+            ->groupBy('m.id');
+
+        $data = $queryBuilder->getQuery()->getResult();
+
+        /** @var $queryFactory QueryFactory */
+        $queryFactory = $this->get('acme_demo_grid.product_query_factory');
+        $queryFactory->setQueryBuilder($queryBuilder);
+
+        /** @var $manufacturerGridManager UserDatagridManager */
+        $manufacturerGridManager = $this->get('acme_demo_grid.product_grid.manager');
+        $datagrid = $manufacturerGridManager->getDatagrid();
+
+        if ('json' == $request->getRequestFormat()) {
+            $view = 'OroGridBundle:Datagrid:list.json.php';
+        } else {
+            $view = 'AcmeDemoGridBundle:User:list.html.twig';
+        }
+        return $this->render(
+            $view,
+            array(
+                'datagrid' => $datagrid,
+                'form'     => $datagrid->getForm()->createView()
+            )
+        );
+    }
+}
---
+++ src/Acme/Bundle/DemoGridBundle/Datagrid/ProductDatagridManager.php
@@ -0,0 +1,230 @@
+<?php
+
+namespace Acme\Bundle\DemoGridBundle\Datagrid;
+
+use Oro\Bundle\GridBundle\Datagrid\DatagridManager;
+use Oro\Bundle\GridBundle\Field\FieldDescription;
+use Oro\Bundle\GridBundle\Field\FieldDescriptionCollection;
+use Oro\Bundle\GridBundle\Field\FieldDescriptionInterface;
+use Oro\Bundle\GridBundle\Filter\FilterInterface;
+
+class ProductDatagridManager extends DatagridManager
+{
+    /**
+     * @var FieldDescriptionCollection
+     */
+    protected $fieldsCollection;
+
+    /**
+     * @return FieldDescriptionCollection
+     */
+    protected function getFieldDescriptionCollection()
+    {
+        if (!$this->fieldsCollection) {
+            $this->fieldsCollection = new FieldDescriptionCollection();
+
+            $fieldManufacturerId = new FieldDescription();
+            $fieldManufacturerId->setName('m_id');
+            $fieldManufacturerId->setOptions(
+                array(
+                    'type'         => FieldDescriptionInterface::TYPE_INTEGER,
+                    'label'        => 'Manufacturer ID',
+                    'field_type'   => FieldDescriptionInterface::TYPE_INTEGER,
+                    'entity_alias' => 'm',
+                    'field_name'   => 'id',
+                    'filter_type'  => FilterInterface::TYPE_NUMBER,
+                    'required'     => false,
+                    'sortable'     => true,
+                    'filterable'   => true,
+                    'show_filter'  => true,
+                )
+            );
+            $this->fieldsCollection->add($fieldManufacturerId);
+
+            $fieldManufacturerName = new FieldDescription();
+            $fieldManufacturerName->setName('m_name');
+            $fieldManufacturerName->setOptions(
+                array(
+                    'type'        => FieldDescriptionInterface::TYPE_TEXT,
+                    'label'        => 'Manufacturer name',
+                    'field_type'   => FieldDescriptionInterface::TYPE_TEXT,
+                    'entity_alias' => 'm',
+                    'field_name'   => 'name',
+                    'filter_type'  => FilterInterface::TYPE_STRING,
+                    'required'     => false,
+                    'sortable'     => true,
+                    'filterable'   => true,
+                    'show_filter'  => true,
+                )
+            );
+            $this->fieldsCollection->add($fieldManufacturerName);
+
+            $fieldManufacturerId = new FieldDescription();
+            $fieldManufacturerId->setName('product_count');
+            $fieldManufacturerId->setOptions(
+                array(
+                    'type'         => FieldDescriptionInterface::TYPE_INTEGER,
+                    'label'        => 'Number of products',
+                    'field_type'   => FieldDescriptionInterface::TYPE_INTEGER,
+                    'field_name'   => 'COUNT(p.id)',
+                    'filter_type'  => FilterInterface::TYPE_NUMBER,
+                    'required'     => false,
+                    'sortable'     => false,
+                    'filterable'   => false,
+                    'show_filter'  => false,
+                )
+            );
+            $this->fieldsCollection->add($fieldManufacturerId);
+
+            $fieldId = new FieldDescription();
+            $fieldId->setName('id');
+            $fieldId->setOptions(
+                array(
+                    'type'        => FieldDescriptionInterface::TYPE_INTEGER,
+                    'label'       => 'Product ID',
+                    'field_type'  => FieldDescriptionInterface::TYPE_INTEGER,
+                    'entity_alias' => 'p',
+                    'field_name'  => 'id',
+                    'filter_type' => FilterInterface::TYPE_NUMBER,
+                    'required'    => false,
+                    'sortable'    => true,
+                    'filterable'  => true,
+                    'show_filter' => true,
+                )
+            );
+            $this->fieldsCollection->add($fieldId);
+
+            $fieldName = new FieldDescription();
+            $fieldName->setName('name');
+            $fieldName->setOptions(
+                array(
+                    'type'        => FieldDescriptionInterface::TYPE_TEXT,
+                    'label'       => 'Name',
+                    'field_type'  => FieldDescriptionInterface::TYPE_TEXT,
+                    'entity_alias' => 'p',
+                    'field_name'  => 'name',
+                    'filter_type' => FilterInterface::TYPE_STRING,
+                    'required'    => false,
+                    'sortable'    => true,
+                    'filterable'  => true,
+                    'show_filter' => true,
+                )
+            );
+            $this->fieldsCollection->add($fieldName);
+
+            $fieldPrice = new FieldDescription();
+            $fieldPrice->setName('price');
+            $fieldPrice->setOptions(
+                array(
+                    'type'        => FieldDescriptionInterface::TYPE_DECIMAL,
+                    'label'       => 'Price',
+                    'field_type'  => FieldDescriptionInterface::TYPE_INTEGER,
+                    'entity_alias' => 'p',
+                    'field_name'  => 'price',
+                    'filter_type' => FilterInterface::TYPE_NUMBER,
+                    'required'    => false,
+                    'sortable'    => true,
+                    'filterable'  => true,
+                    'show_filter' => true,
+                )
+            );
+            $this->fieldsCollection->add($fieldPrice);
+
+            $fieldCount = new FieldDescription();
+            $fieldCount->setName('count');
+            $fieldCount->setOptions(
+                array(
+                    'type'        => FieldDescriptionInterface::TYPE_INTEGER,
+                    'label'       => 'Count',
+                    'field_type'  => FieldDescriptionInterface::TYPE_INTEGER,
+                    'entity_alias' => 'p',
+                    'field_name'  => 'count',
+                    'filter_type' => FilterInterface::TYPE_NUMBER,
+                    'required'    => false,
+                    'sortable'    => true,
+                    'filterable'  => true,
+                    'show_filter' => true,
+                )
+            );
+            $this->fieldsCollection->add($fieldCount);
+
+            $fieldDescription = new FieldDescription();
+            $fieldDescription->setName('description');
+            $fieldDescription->setOptions(
+                array(
+                    'type'        => FieldDescriptionInterface::TYPE_TEXT,
+                    'label'       => 'Description',
+                    'field_type'  => FieldDescriptionInterface::TYPE_TEXT,
+                    'entity_alias' => 'p',
+                    'field_name'  => 'description',
+                    'filter_type' => FilterInterface::TYPE_STRING,
+                    'required'    => false,
+                    'sortable'    => true,
+                    'filterable'  => true,
+                    'show_filter' => true,
+                )
+            );
+            $this->fieldsCollection->add($fieldDescription);
+
+            $fieldCreateDate = new FieldDescription();
+            $fieldCreateDate->setName('createDate');
+            $fieldCreateDate->setOptions(
+                array(
+                    'type'        => FieldDescriptionInterface::TYPE_DATETIME,
+                    'label'       => 'Create Date',
+                    'field_type'  => FieldDescriptionInterface::TYPE_DATETIME,
+                    'entity_alias' => 'p',
+                    'field_name'  => 'createDate',
+                    'filter_type' => FilterInterface::TYPE_DATETIME,
+                    'required'    => false,
+                    'sortable'    => true,
+                    'filterable'  => true,
+                    'show_filter' => true,
+                )
+            );
+            $this->fieldsCollection->add($fieldCreateDate);
+        }
+
+        return $this->fieldsCollection;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getListFields()
+    {
+        return $this->getFieldDescriptionCollection()->getElements();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getSorters()
+    {
+        $fields = array();
+        /** @var $fieldDescription FieldDescription */
+        foreach ($this->getFieldDescriptionCollection() as $fieldDescription) {
+            if ($fieldDescription->getOption('sortable')) {
+                $fields[] = $fieldDescription;
+            }
+        }
+
+        return $fields;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFilters()
+    {
+        $fields = array();
+        /** @var $fieldDescription FieldDescription */
+        foreach ($this->getFieldDescriptionCollection() as $fieldDescription) {
+            if ($fieldDescription->getOption('filterable')) {
+                $fields[] = $fieldDescription;
+            }
+        }
+
+        return $fields;
+    }
+}
