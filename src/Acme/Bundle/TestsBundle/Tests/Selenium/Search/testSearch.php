8137312dbc4ed38c21433d20a710de8e6dee6353
Michael Banin <mbanin@magecore.com>
2013-04-02 15:01:28 +0300

    BAP-347 Create test for Simple Search - create Selenium2 scripts for search suggestions drop-down

--- src/Acme/Bundle/TestsBundle/Tests/Selenium/Search/testSearch.php
+++ src/Acme/Bundle/TestsBundle/Tests/Selenium/Search/testSearch.php
@@ -25,4 +25,26 @@ class TestSearchForm extends \PHPUnit_Extensions_Selenium2TestCase
         $searchResult = $this->byXPath("//*[@id='main']/h2[contains(.,'Sorry, there are no results')]")->text();
         $this->assertEquals('Sorry, there are no results for your request', $searchResult);
     }
+
+    public function testSearchSuggestions()
+    {
+        $this->byId('prependedInput')->value(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_LOGIN);
+        $this->byId('prependedInput2')->value(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_PASS);
+        $this->clickOnElement('_submit');
+
+        $this->byId('search-bar-search')->value('zxcv-');
+        $this->timeouts()->implicitWait(10000);
+
+        $this->assertTrue($this->isElementPresent("//*[@id='search-dropdown']/ul/li"), 'No search suggestions available');
+    }
+
+    public function isElementPresent($locator)
+    {
+        try {
+            $this->byXPath($locator);
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
 }
