965483157cb82443db310128bed7566b2b7da42e
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2013-04-05 12:17:28 +0300

    BAP-175: Date / Datetime filter type  - removed flexible datetime attribute  - updated submodule reference

--- src/Acme/Bundle/DemoGridBundle/DataFixtures/ORM/LoadUserData.php
+++ src/Acme/Bundle/DemoGridBundle/DataFixtures/ORM/LoadUserData.php
@@ -77,10 +77,10 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, C
             $this->persist($hobbyAttribute);
         }
 
-        if (!$this->findAttribute('last_visit')) {
-            $lastVisitAttribute = $this->createAttribute(new DateTimeType(), 'last_visit');
-            $this->persist($lastVisitAttribute);
-        }
+        // if (!$this->findAttribute('last_visit')) {
+        //     $lastVisitAttribute = $this->createAttribute(new DateTimeType(), 'last_visit');
+        //     $this->persist($lastVisitAttribute);
+        // }
 
         $this->flush();
     }
@@ -196,7 +196,7 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, C
         $this->setFlexibleAttributeValueOption($user, 'gender', $gender);
         $this->setFlexibleAttributeValue($user, 'website', $website);
         $this->addFlexibleAttributeValueOptions($user, 'hobby', $hobbies);
-        $this->setFlexibleAttributeValue($user, 'last_visit', $lastVisit);
+        // $this->setFlexibleAttributeValue($user, 'last_visit', $lastVisit);
 
         return $user;
     }
