599f4a1c5cef0a1629f5b96f1edf57abded85444
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-04-09 12:40:15 +0300

    BAP-198 Create REST test for advanced search  - CR changes in tests config

--- src/Acme/Bundle/TestsBundle/Test/ScreenshotListener.php
+++ src/Acme/Bundle/TestsBundle/Test/ScreenshotListener.php
@@ -45,9 +45,28 @@ class ScreenshotListener implements \PHPUnit_Framework_TestListener
         }
     }
 
-    public function addIncompleteTest(\PHPUnit_Framework_Test $test, \Exception $e, $time) {}
-    public function addSkippedTest(\PHPUnit_Framework_Test $test, \Exception $e, $time) {}
-    public function startTest(\PHPUnit_Framework_Test $test) {}
-    public function startTestSuite(\PHPUnit_Framework_TestSuite $suite) {}
-    public function endTestSuite(\PHPUnit_Framework_TestSuite $suite) {}
+    public function addIncompleteTest(\PHPUnit_Framework_Test $test, \Exception $e, $time)
+    {
+
+    }
+
+    public function addSkippedTest(\PHPUnit_Framework_Test $test, \Exception $e, $time)
+    {
+
+    }
+
+    public function startTest(\PHPUnit_Framework_Test $test)
+    {
+
+    }
+
+    public function startTestSuite(\PHPUnit_Framework_TestSuite $suite)
+    {
+
+    }
+
+    public function endTestSuite(\PHPUnit_Framework_TestSuite $suite)
+    {
+
+    }
 }
