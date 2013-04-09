c088ddd2cc2f477c2139f253c99342df6b83c201
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-04-09 18:14:05 +0300

    BAP-347 Create test for Simple Search  - CR Changes

--- src/Acme/Bundle/TestsBundle/Tests/Selenium/Search/SimpleSearchTest.php
+++ src/Acme/Bundle/TestsBundle/Tests/Selenium/Search/SimpleSearchTest.php
@@ -16,23 +16,6 @@ class SimpleSearchTest extends \PHPUnit_Extensions_Selenium2TestCase
         $this->cookie()->clear();
     }
 
-    public function testSearchSuggestions()
-    {
-        $this->timeouts()->implicitWait(10000);
-        $this->url('user/login');
-        //log-in
-        $this->byId('prependedInput')->value(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_LOGIN);
-        $this->byId('prependedInput2')->value(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_PASS);
-        $this->clickOnElement('_submit');
-        //fill-in simple search field
-        $this->byId('search-bar-search')->value('admin@example.com');
-        //checking that search suggestion drop-down available or not
-        $this->assertTrue(
-            $this->isElementPresent("//*[@id='search-dropdown']/ul/li"),
-            'No search suggestions available'
-        );
-    }
-
     public function isElementPresent($locator)
     {
         try {
@@ -43,16 +26,47 @@ class SimpleSearchTest extends \PHPUnit_Extensions_Selenium2TestCase
         }
     }
 
-    public function testSearchResult()
+    protected function waitPageToLoad()
     {
-        $this->timeouts()->implicitWait(10000);
+        $script = "return document['readyState']";
+        sleep(1);
+        while ('complete'!= $this->execute(array('script' => $script, 'args' => array()))) {
+            //empty loop
+            sleep(1);
+        };
+        $this->timeouts()->implicitWait(self::TIME_OUT);
+    }
+
+    public function testSearchSuggestions()
+    {
         $this->url('user/login');
+        $this->waitPageToLoad();
+        //log-in
         $this->byId('prependedInput')->value(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_LOGIN);
         $this->byId('prependedInput2')->value(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_PASS);
         $this->clickOnElement('_submit');
+        $this->waitPageToLoad();
+        //fill-in simple search field
+        $this->byId('search-bar-search')->value('admin@example.com');
+        //checking that search suggestion drop-down available or not
+        $this->assertTrue(
+            $this->isElementPresent("//*[@id='search-dropdown']/ul/li"),
+            'No search suggestions available'
+        );
+    }
+
+    public function testSearchResult()
+    {
+        $this->url('user/login');
+        $this->waitPageToLoad();
+        $this->byId('prependedInput')->value(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_LOGIN);
+        $this->byId('prependedInput2')->value(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_PASS);
+        $this->clickOnElement('_submit');
+        $this->waitPageToLoad();
 
         $this->byId('search-bar-search')->value('admin');
         $this->byXPath("//*[@id='search-div']//div/button[contains(.,'Search')]")->click();
+        $this->waitPageToLoad();
 
         $searchResult = $this->byXPath("//div[@class='container-fluid']/div/h3/a[contains(., 'admin')]")->text();
         $this->assertEquals('admin', $searchResult);
