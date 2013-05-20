25eccb467852908fd008d35c0993ddebbf63295a
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-05-20 13:32:17 +0300

    BAP-795  Fix selenium tests for latest UI changes  - fixed seleniun tests

--- src/Acme/Bundle/TestsBundle/Tests/Selenium/Grids/GridTest.php
+++ src/Acme/Bundle/TestsBundle/Tests/Selenium/Grids/GridTest.php
@@ -95,10 +95,10 @@ class GridTest extends \PHPUnit_Extensions_Selenium2TestCase
     public function filterData()
     {
         return array(
-            'ID' => array('ID', '='),
+            //'ID' => array('ID', '='),
             'Username' => array('Username', 'is equal to'),
             'Email' => array('Email', 'contains'),
-            //'First name' => array('First name', 'is equal to'),
+            'First name' => array('First name', 'is equal to'),
             //'Birthday' => array('Birthday', '')
         );
     }
