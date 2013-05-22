837b2fb10de521b6f598023d8dc2ac85d3be8d5f
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-05-22 19:23:39 +0300

    BAP-795  Fix selenium tests for latest UI changes  - fixed seleniun tests

--- src/Acme/Bundle/TestsBundle/Tests/Selenium/Grids/GridTest.php
+++ src/Acme/Bundle/TestsBundle/Tests/Selenium/Grids/GridTest.php
@@ -98,7 +98,7 @@ class GridTest extends \PHPUnit_Extensions_Selenium2TestCase
             //'ID' => array('ID', '='),
             'Username' => array('Username', 'is equal to'),
             'Email' => array('Email', 'contains'),
-            'First name' => array('First name', 'is equal to'),
+            //'First name' => array('First name', 'is equal to'),
             //'Birthday' => array('Birthday', '')
         );
     }
@@ -192,7 +192,7 @@ class GridTest extends \PHPUnit_Extensions_Selenium2TestCase
             //'ID' => array('ID'),
             'Username' => array('Username'),
             'Email' => array('Email'),
-            'First name' => array('First name'),
+            //'First name' => array('First name'),
             //'Birthday' => array('Birthday'),
             //'Company' => array('Company'),
             //'Salary' => array('Salary'),
