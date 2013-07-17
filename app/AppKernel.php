9f0824af1f2b1d583d99de4ce0373877d0a3c9ca
Ignat Shcheglovskyi <ignat.scheglovskiy@gmail.com>
2013-07-17 18:11:08 +0300

    BAP-1211: Create OroWorkflowBundle and basic entities - added OroWorkflowBundle into AppKernel and routing.yml

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -56,6 +56,7 @@ class AppKernel extends Kernel
             new Oro\Bundle\TagBundle\OroTagBundle(),
             new Oro\Bundle\AsseticBundle\OroAsseticBundle(),
             new Oro\Bundle\TranslationBundle\OroTranslationBundle(),
+            new Oro\Bundle\WorkflowBundle\OroWorkflowBundle(),
 
             // BAP Demo bundles
             new Acme\Bundle\DemoBundle\AcmeDemoBundle(),
