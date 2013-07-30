e4d626f1826da2f450aec79ad710cb48b793b269
Alexander Nezdoiminoga <alex@magecore.com>
2013-07-30 17:13:13 +0300

    fix install.bat

--- install.bat
+++ install.bat
@@ -8,7 +8,7 @@ php app/console-framework oro:search:create-index --env %ENV%
 php app/console-framework doctrine:fixture:load --no-debug --no-interaction --env %ENV%
 php app/console-framework oro:acl:load --env %ENV%
 php app/console-framework oro:navigation:init --env %ENV%
-php app/console-framework oro:entity-config:update --env $ENV
+php app/console-framework oro:entity-config:update --env %ENV%
 php app/console-framework assets:install web --env %ENV%
 php app/console assetic:dump --env %ENV%
 php app/console oro:assetic:dump
