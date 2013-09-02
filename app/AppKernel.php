2882b556912ce9ab9c5da900452bc9f383fe2be3
Dan Yasnyuk <danil@magecore.com>
2013-09-02 13:59:36 +0300

    JMSJobQueue bundle installation fixed

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             //new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            //new JMS\JobQueueBundle\JMSJobQueueBundle(),
+            new JMS\JobQueueBundle\JMSJobQueueBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle($this),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new FOS\RestBundle\FOSRestBundle(),
