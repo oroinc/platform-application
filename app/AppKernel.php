9c62c9ca55da3386e7944188e41675c8aa5cf8e6
Dan Yasnyuk <danil@magecore.com>
2013-09-03 15:35:02 +0300

    BAP-1365: Develop WAMP server application for broadcasting client data updates (PHP side) - wesocket server config moved to parameters.yml

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -64,6 +64,7 @@ class AppKernel extends Kernel
             new Oro\Bundle\EntityBundle\OroEntityBundle(),
             new Oro\Bundle\EntityConfigBundle\OroEntityConfigBundle(),
             new Oro\Bundle\EntityExtendBundle\OroEntityExtendBundle(),
+            new Oro\Bundle\CronBundle\OroCronBundle(),
             new Oro\Bundle\SyncBundle\OroSyncBundle(),
 
             // BAPP bundles
