0ef723f6e7fb0988b310780de5c287591451cb52
Alexander Nezdoiminoga <alex@magecore.com>
2013-09-25 23:03:06 +0300

    update install scripts

--- install.bat
+++ install.bat
@@ -7,6 +7,7 @@ if "%1" NEQ "" (
 php app/console-framework oro:entity-extend:clear --env %ENV% || goto :error
 php app/console-framework doctrine:schema:create --env %ENV% || goto :error
 php app/console-framework doctrine:fixture:load --no-debug --no-interaction --env %ENV% || goto :error
+php app/console-framework doctrine:fixtures:load --fixtures=src/Oro/src/Oro/Bundle/TestFrameworkBundle/Fixtures/ --append --no-debug --no-interaction --env %ENV% || goto :error
 php app/console-framework oro:navigation:init --env %ENV% || goto :error
 php app/console-framework oro:entity-config:init --env %ENV% || goto :error
 php app/console-framework oro:entity-extend:init --env %ENV% || goto :error
