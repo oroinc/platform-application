b4a9512bef4ae38bf9449c5f63f9a7e671c2e5ef
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2013-10-08 16:37:20 +0300

    Implemented BAP-1839: Invalidate require.js config once cache was removed

--- install.sh
+++ install.sh
@@ -20,5 +20,4 @@ php app/console-framework assets:install web --env $ENV
 php app/console-framework assetic:dump --env $ENV
 php app/console-framework oro:assetic:dump --env $ENV
 php app/console-framework oro:translation:dump --env $ENV
-php app/console-framework oro:requirejs:config --env $ENV
 php app/console-framework oro:requirejs:build --env $ENV
