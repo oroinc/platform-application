b77523726acbb085e676b22934f692238ba3dfe7
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-10-31 18:39:00 +0200

    BAP-498: Investigate failed builds and tests  - fixed console in install.bat

--- install.bat
+++ install.bat
@@ -5,7 +5,7 @@ if "%1" NEQ "" (
 )
 
 php app/console-framework oro:entity-extend:clear --env %ENV% || goto :error
-php app/console doctrine:schema:drop --force --full-database --env %ENV% || goto :error
+php app/console-framework doctrine:schema:drop --force --full-database --env %ENV% || goto :error
 php app/console-framework doctrine:schema:create --env %ENV% || goto :error
 php app/console-framework oro:entity-config:init --env %ENV% || goto :error
 php app/console-framework oro:entity-extend:init --env %ENV% || goto :error
@@ -16,10 +16,10 @@ php app/console-framework doctrine:fixtures:load --fixtures=src/Oro/src/Oro/Bund
 php app/console-framework oro:navigation:init --env %ENV% || goto :error
 php app/console-framework oro:search:create-index --env %ENV% || goto :error
 php app/console-framework assets:install web --env %ENV% || goto :error
-php app/console assetic:dump --env %ENV% || goto :error
-php app/console oro:assetic:dump --env %ENV%|| goto :error
-php app/console oro:translation:dump --env %ENV% || goto :error
-php app/console oro:requirejs:build --env %ENV% || goto :error
+php app/console-framework assetic:dump --env %ENV% || goto :error
+php app/console-framework oro:assetic:dump --env %ENV%|| goto :error
+php app/console-framework oro:translation:dump --env %ENV% || goto :error
+php app/console-framework oro:requirejs:build --env %ENV% || goto :error
 goto :EOF
 
 :error
