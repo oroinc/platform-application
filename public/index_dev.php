ab03f8d2f850bdd6c39400c098fda72a0000655b
Andrey Yatsenco <ayatsenko@oroinc.com>
2018-05-15 15:06:35 +0300

    BAP-17040: Update application structure to Symfony 4 (#18137)
    * BAP-17040: Update application structure to Symfony 4

--- public/index_dev.php
+++ public/index_dev.php
@@ -23,11 +23,11 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/../var/bootstrap.php.cache';
+
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
